9d1b2019118b6321d432706b8812d9177c8e83e5
mikolov_sergey <sergeymikolov@gmail.com>
2017-12-05 15:26:42 +0200

    master start

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app light>
+    <v-content>
+        <nav-bar></nav-bar>
+        <router-view></router-view>
+    </v-content>
+<footer-bar></footer-bar>
+  </v-app>
+</template>
+
+<script>
+  import NavBar from '@/components/menu/NavBar';
+  import FooterBar from '@/components/footer/FooterBar';
+
+  export default {
+  components: {
+      NavBar,
+      FooterBar
+  },
+    data () {
+      return {
+      }
+    },
+
+  }
+</script>
---
+++ src/components/footer/FooterBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer :fixed="false" app>
+    <span>&copy; 2017</span>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+      data () {
+          return {
+              name: 'FooterBar'
+          }
+      }
+  }
+</script>
+
+<style scoped>
+
+</style>
